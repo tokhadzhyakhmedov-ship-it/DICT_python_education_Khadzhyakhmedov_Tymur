8242bee3fdd7a6f4dc3e7c8e3acadcf4816eaf34
tokhadzhyakhmedov-ship-it <t.o.khadzhyakhmedov@student.khai.edu>
2026-04-09 22:54:15 +0300

    stage final: dinnerparty

---
+++ dinnerparty/dinnerparty.py
@@ -0,0 +1,30 @@
+import random
+print("Enter the number of friends joining (except you):")
+number_of_friends = int(input())
+if number_of_friends <= 0:
+    print("you are lonely (")
+else:
+    print("Enter the name of every friend (including you), each on a new line:")
+    friends = {}
+    for i in range(number_of_friends):
+        name = input()
+        friends[name] = 0
+    print("Enter the total amount:")
+    total_amount = float(input())
+    share = round(total_amount / number_of_friends, 2)
+    for name in friends:
+        friends[name] = share
+    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+    lucky_answer = input()
+    if lucky_answer == "Yes":
+        lucky_friend = random.choice(list(friends.keys()))
+        print(f"{lucky_friend} is lucky one!")
+        new_share = round(total_amount / (number_of_friends - 1), 2)
+        for name in friends:
+            if name == lucky_friend:
+                friends[name] = 0
+            else:
+                friends[name] = new_share
+    else:
+        print("No one is going to be lucky")
+    print(friends)
